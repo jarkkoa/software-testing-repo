d02202a809ca263111dc4430f9f4df3bf390b8b8
Jarkko Ahvenniemi <jarge98@gmail.com>
2023-12-07 18:23:07 +0200

    Update example test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,10 @@
 import assert from "assert";
+import add from "../library/src/add.js"
 
 describe("Example test", function () {
   describe("#add()", function () {
     it("Should add two integers together", function () {
-      assert.equal((1+2), 3);
+      assert.equal(add(1,2), 3);
     });
   });
 });
