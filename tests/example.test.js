8267046eedb83bc651957ed45827575e68586a02
Jarkko Ahvenniemi <jarge98@gmail.com>
2023-12-07 18:12:39 +0200

    Add an example test

---
+++ tests/example.test.js
@@ -0,0 +1,9 @@
+import assert from "assert";
+
+describe("Example test", function () {
+  describe("#add()", function () {
+    it("Should add two integers together", function () {
+      assert.equal((1+2), 3);
+    });
+  });
+});
